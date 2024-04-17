d419dcf33ff874b398a4513fc40e57899b4ae45a
VenkatGupta31 <venkateshwarlu.gundla@preqin.com>
2024-04-18 00:47:17 +0530

    sign in sign out from client n server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-"use client";
-
 import { Button } from "@nextui-org/react";
+import Profile from "@/components/Profile";
+import * as actions from "@/actions";
+import { auth } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
-      <Button>Click me..</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign in</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed out </div>
+      )}
+
+      <Profile />
     </div>
   );
 }
