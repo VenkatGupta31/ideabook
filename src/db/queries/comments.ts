b7315fbd0fecfcce5cc83a5791c35eea758c43af
VenkatGupta31 <venky_leo31@outlook.com>
2024-04-25 03:40:37 +0530

    implement caching mehanism

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,23 +1,26 @@
 import type { Comment } from "@prisma/client";
 
 import { db } from "@/db";
+import { log } from "console";
+import { cache } from "react";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentsByPostId(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  return db.comment.findMany({
-    where: { postId },
-    include: {
-      user: {
-        select: {
-          name: true,
-          image: true,
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    log("making a query");
+    return db.comment.findMany({
+      where: { postId },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
         },
       },
-    },
-  });
-}
+    });
+  }
+);
