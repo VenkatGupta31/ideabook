121045a68ae7182faf5a9cf562778d319f2a4d7f
VenkatGupta31 <venky_leo31@outlook.com>
2024-04-27 01:36:32 +0530

    top ideas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import IdeaCreateForm from "@/components/ideas/idea-create-form";
 import IdeaList from "@/components/ideas/idea-list";
+import PostList from "@/components/posts/post-list";
+import { fetchTopPosts } from "@/db/queries/posts";
 import { Divider } from "@nextui-org/react";
 
 export default function Home() {
@@ -7,6 +9,7 @@ export default function Home() {
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-xl m-2">Top ideas</h1>
+        <PostList fetchData={fetchTopPosts} />
       </div>
       <div className="border shodow py-3 px-2">
         <IdeaCreateForm />
